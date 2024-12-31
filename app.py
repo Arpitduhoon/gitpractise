24bb5a3eb1e351e6453cf1b4fdfc84ff516a5623
Arpit duhoon <arpitduhoon17@gmail.com>
2024-12-31 14:33:13 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+
+
+from src.person import Person
+from src.car import Car
+
+if __name__=="__main__":
+    person = Person("Arpit","Duhoon")
+    person.display()
+    car=Car("BMW","EV")
+    car.display()
+
+
+## positional and keyword Arguments
+ 
